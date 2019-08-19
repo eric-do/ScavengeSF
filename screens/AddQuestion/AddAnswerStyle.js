70e69a9519d59e3da287e147ade89fecc9222577
ericdo <ericdo.617@gmail.com>
2019-08-19 15:15:43 -0700

    Refactor AddAnswer to be aligned with ESLint AirBNB/React-Hooks

---
+++ screens/AddQuestion/AddAnswerStyle.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    flex: 1,
+  },
+  textInput: {
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
+  picker: {
+    width: '75%',
+  },
+  nextButton: {
+    marginTop: 10,
+    backgroundColor: '#70EB92',
+  },
+  doneButton: {
+    marginTop: 10,
+    backgroundColor: '#2AD7C2',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+  },
+});
